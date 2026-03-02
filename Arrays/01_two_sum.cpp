44efc7a4c92c8cb62cf15a638a74c412d48b84e4
Shiva0454 <Shivajakhad1@gmail.com>
2026-03-02 21:51:03 +0530

    Added two sum problem in C++

---
+++ Arrays/01_two_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,4,5};
+    int n=4;
+    int target=9;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                cout<<i << " "<< j;
+                return 0;
+            }
+        }
+    }
+return 0;
+}
