7d36b6036a4adcd3d50e5b1463ffc2cbf2f1c976
Shiva0454 <Shivajakhad1@gmail.com>
2026-03-18 23:14:56 +0530

    solved three sum

---
+++ Arrays/21_3sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        sort(nums.begin(), nums.end());  // Step 1: sort
+        
+        for (int i = 0; i < nums.size() - 2; i++) {
+            
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.size() - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;   // need bigger sum
+                }
+                else {
+                    right--;  // need smaller sum
+                }
+            }
+        }
+        
+        return result;
+    }
+};
